a1b1576e0e8f1793bd05793394a7d5494f5615f5
Mads Risager <madssr2@gmail.com>
2024-10-24 12:37:27 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+# Use the official TensorFlow image with GPU support
+FROM tensorflow/tensorflow:latest-gpu
+
+# Set the working directory
+WORKDIR /app
+
+# Install additional dependencies if needed
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    libssl-dev \
+    libffi-dev \
+    wget \
+    git \
+    && apt-get clean
+
+
+COPY requirements.txt /app/requirements.txt
+
+# Install specific Python packages
+RUN pip install --upgrade pip
+RUN pip install --no-cache-dir --ignore-installed -r requirements.txt
+
+
+# Copy the local files to the container
+COPY . /app
+
+# Expose the port (optional, depending on your application)
+EXPOSE 8888
+
+# Command to run your app (e.g., a Python script or Jupyter Notebook)
+CMD ["python", "src/app.py", "--port", "8888"]
---
+++ src/app.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+import argparse
+
+# import json
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@tf.keras.utils.register_keras_serializable()
+def dist_euclidean(y_true, y_pred):
+    return tf.keras.backend.mean(
+        tf.keras.backend.sqrt(
+            tf.keras.backend.sum(
+                tf.keras.backend.square(y_pred - y_true), axis=-1, keepdims=True
+            )
+            + 1e-16
+        ),
+        axis=-1,
+    )
+
+
+@app.route("/")
+def model_info():
+    return jsonify(
+        {
+            "model": model_path,
+            "input_shape": config["layers"][0]["config"]["batch_shape"],
+            "gpus_available": tf.config.list_physical_devices("GPU"),
+        }
+    )
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    if not request.json:
+        return jsonify({"error": "No data provided"})
+
+    data: dict = request.json
+
+    inputs = tf.constant(data.get("data"), dtype=tf.float32)
+
+    pred = model.predict(inputs)
+
+    return jsonify({"prediction": pred.tolist()})
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model-path", "-m", type=str, required=False)
+    parser.add_argument("--debug", action="store_true", required=False)
+    parser.add_argument("--port", type=int, default=5000)
+    parser.add_argument("--host", type=str, default="0.0.0.0")
+    args = parser.parse_args()
+
+    model_path = "rd9_epoch100_h1n350_ffn150_final_model.keras"
+    if args.model_path:
+        model_path = args.model_path
+
+    model = tf.keras.models.load_model(
+        model_path, custom_objects={"dist_euclidean": dist_euclidean}
+    )
+
+    config = model.get_config()
+
+    app.run(
+        debug=args.debug,
+        port=args.port,
+        host=args.host,
+    )
